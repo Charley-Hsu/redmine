9372671556bda3b2f0f61b322f99795e7e4e4501
charley-Hsu <mail@90vip.net>
2018-03-08 15:35:19 +0800

    删除无用文件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <!-- APP.vue -->
 <template>
   <div id="app">
-<!--     <el-carousel :interval="4000" type="card" height="100px">
-      <el-carousel-item v-for="item in imgList" :key="item">
-        <img :src="item" class="logoImg">
-      </el-carousel-item>
-    </el-carousel> -->
     <router-view></router-view> <!-- 原本的Login换成了router-view 这就是路由视图渲染的目标元素-->
   </div>
 </template>
@@ -13,7 +8,7 @@
   export default {
     data: function () {
       return {
-        imgList: ['./static/node.png', './static/vue.png', './static/element.png', './static/music2.jpg']
+
       }
     },
     name: 'app' // 不需要再引入`Login`组件了，因为在路由里已经注册了
